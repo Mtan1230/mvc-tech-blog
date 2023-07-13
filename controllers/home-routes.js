45486f8b7f81ea3c5022a053054174bc885b8622
Mtan1230 <m.tan1230@gmail.com>
2023-07-13 15:39:19 -0400

    add helper function to display date

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,7 +15,6 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) =>
             post.get({ plain: true })
         );
-        console.log({posts})
         res.render('homepage', {
             posts,
         });
