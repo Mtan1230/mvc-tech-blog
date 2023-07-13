096960da9a2c5b5954af6f2e614b8f64cec4539e
Mtan1230 <m.tan1230@gmail.com>
2023-07-13 15:07:17 -0400

    add seeds

---
+++ seeds/userData.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    username: 'JDoe',
+    email: 'j.doe@gmail.com',
+    password: '123456'
+  }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
